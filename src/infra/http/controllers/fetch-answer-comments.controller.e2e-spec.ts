3e77589c06b5428315b5c8ed5c7cb0ef4b4b33ec
Erik Garces Matos <erik_senac@hotmail.com>
2025-02-13 13:56:22 -0300

    feat: Creating answer comment with author reponse

--- src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-answer-comments.controller.e2e-spec.ts
@@ -39,7 +39,9 @@ describe('Fetch answer comments (E2E)', () => {
   })
 
   test('[GET] /answers/:answerId/comments', async () => {
-    const user = await studentFactory.makePrismaStudent()
+    const user = await studentFactory.makePrismaStudent({
+      name: 'John Doe',
+    })
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
@@ -78,6 +80,18 @@ describe('Fetch answer comments (E2E)', () => {
       comments: expect.arrayContaining([
         expect.objectContaining({ content: 'Comment 1' }),
         expect.objectContaining({ content: 'Comment 2' }),
+      ]),
+    })
+    expect(response.body).toEqual({
+      comments: expect.arrayContaining([
+        expect.objectContaining({
+          content: 'Comment 1',
+          authorName: 'John Doe',
+        }),
+        expect.objectContaining({
+          content: 'Comment 2',
+          authorName: 'John Doe',
+        }),
       ]),
     })
   })
