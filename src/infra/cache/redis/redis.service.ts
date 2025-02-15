7172fcb7c7e77bf808742617ea921dc64a33072e
Erik Garces Matos <erik_senac@hotmail.com>
2025-02-15 12:28:54 -0300

    feat: Addin ssl for redis connection

--- src/infra/cache/redis/redis.service.ts
+++ src/infra/cache/redis/redis.service.ts
@@ -5,11 +5,12 @@ import { Redis } from 'ioredis'
 @Injectable()
 export class RedisService extends Redis implements OnModuleDestroy {
   constructor(envService: EnvService) {
-    const useTLS = envService.get('REDIS_USE_TLS') === 'true'
+    const useTLS = envService.get('REDIS_USE_TLS') === 'true' // only in production
     super({
       host: envService.get('REDIS_HOST'),
       port: envService.get('REDIS_PORT'),
       db: envService.get('REDIS_DB'),
+      password: useTLS ? envService.get('REDIS_PASSWORD') : undefined,
       tls: useTLS ? {} : undefined,
     })
   }
